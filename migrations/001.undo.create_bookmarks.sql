16022ed36f793ccb9e784fb821181fe38c62cab4
Glaiza Wagner <glaizawagner@gmail.com>
2019-11-07 11:26:16 -0700

    small changes

--- migrations/001.undo.create_bookmarks.sql
+++ migrations/001.undo.create_bookmarks.sql
@@ -1,1 +1,1 @@
-DROP TABLE IF EXISTS bookmarks
+DROP TABLE IF EXISTS bookmarks;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
+/* eslint-disable no-console */
+const knex = require('knex');
 const app = require('./app');
-const { PORT } = require('./config');
+const { PORT, DB_URL } = require('./config');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL
+});
+
+//use to set the Knex instance
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
