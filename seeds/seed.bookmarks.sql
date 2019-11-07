0082b2c5c48561800b96072cea16d2d7ff900cf1
Nathan Leung <nath.leung@gmail.com>
2019-11-07 09:42:00 -0800

    some updates

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,5 @@
+INSERT INTO bookmarks(title,url, description, rating)
+VALUES
+    ('Google','https://www.google.com','Google search',5),
+    ('Youtube','https://www.Youtube.com','watch vidoes',4)
+;
